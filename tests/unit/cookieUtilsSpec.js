2d01ad9a55f91d1838157d5e6cc2d487d5389120
Gareth Rogers <grogers@thoughtworks.com>
2013-03-01 14:02:03 +0000

    Made test values json compliant

--- tests/unit/cookieUtilsSpec.js
+++ tests/unit/cookieUtilsSpec.js
@@ -1,21 +1,20 @@
 describe("cookie utils", function () {
   
   // BEFORE ALL
-  document.cookie = "foo=bar";
-  document.cookie = " zap = pow ";
-  document.cookie = " zig=zag";
+  document.cookie = 'foo="bar"';
+  document.cookie = ' zap = "pow" ';
+  document.cookie = ' zig="zag"';
   
   it("should extract cookies as key values with trimmed white space", function () {
-    console.log(cookieUtils.cookiesAsKeyValues());
-    expect(cookieUtils.cookiesAsKeyValues()).toContain({ key: 'foo', value: 'bar' });
-    expect(cookieUtils.cookiesAsKeyValues()).toContain({ key: 'zap', value: 'pow' });
-    expect(cookieUtils.cookiesAsKeyValues()).toContain({ key: 'zig', value: 'zag' });
+    expect(cookieUtils.cookiesAsKeyValues()).toContain({ key: 'foo', value: '"bar"' });
+    expect(cookieUtils.cookiesAsKeyValues()).toContain({ key: 'zap', value: '"pow"' });
+    expect(cookieUtils.cookiesAsKeyValues()).toContain({ key: 'zig', value: '"zag"' });
   });
   
   it("should extract a single cookie by name", function () {
     var retrivedCookie = cookieUtils.getCookieNamed('zap');
     expect(retrivedCookie.key).toBe('zap');
-    expect(retrivedCookie.value).toBe('pow');
+    expect(retrivedCookie.value).toBe('"pow"');
   });
   
 });
