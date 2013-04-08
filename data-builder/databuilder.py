500a06e367cd51c8935e3c472d276911eeeb5ea8
Gareth Rogers <grogers@thoughtworks.com>
2013-04-08 12:09:54 +0100

    Send json to write api
    @pbadenski
    @gtrogers

--- data-builder/databuilder.py
+++ data-builder/databuilder.py
@@ -1,11 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import requests
 
 app = Flask(__name__)
 
+app.debug = True
 
 @app.route("/")
 def main_page():
     return render_template('index.html')
+
+
+@app.route("/post", methods=['POST'])
+def post_to_platform():
+    url = request.json['writeApiLocation']
+    data = request.json['jsonToPost']
+    req = requests.post(url, data,
+                        headers={'content-type': 'application/json'})
+    if req.status_code is 200:
+        return "thanks"
+    else:
+        return req.text, req.status_code
 
 
 if __name__ == "__main__":
---
+++ data-builder/static/assets/script/dataBuilder.js
@@ -0,0 +1,22 @@
+$(function () {
+    $('#go').on('click', function () {
+        var dataToPost = {
+            writeApiLocation: $('#write-api-location').val(),
+            jsonToPost: editor.getValue()
+        };
+        $.ajax({
+            url: "/post",
+            type: "POST",
+            data: JSON.stringify(dataToPost),
+            contentType: "application/json",
+            processData: false,
+            success: function () {
+                $('#results').append("<p><span class='label label-success'>Post Successful</span></p>")
+            },
+            error: function (req) {
+                console.log(req);
+                $('#results').append("<p><span class='label label-warning'>Post Failed</span></p><p>" + req.responseText +"</p>")
+            }
+        });
+    });
+});
