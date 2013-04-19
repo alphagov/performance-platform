fc29c199c23aa2158b4121b7ea0ae39aa8486600
Gareth <grogers@thoughtworks.com>
2013-04-19 11:39:48 +0100

    Return better error to client when request blows up

--- data-builder/databuilder.py
+++ data-builder/databuilder.py
@@ -1,4 +1,6 @@
+from pprint import pprint
 from flask import Flask, render_template, request
+import json
 import requests
 
 app = Flask(__name__)
@@ -15,18 +17,20 @@ def post_to_platform():
     url = request.json['writeApiLocation']
     data = request.json['jsonToPost']
     bearerToken = request.json.get('bearerToken')
-    print bearerToken, "<----------------------------"
     headers = {'content-type': 'application/json'}
 
     if bearerToken is not None:
         headers.update({'Authorization': 'Bearer ' + bearerToken})
 
-    req = requests.post(url, data,
-                        headers=headers)
-    if req.status_code is 200:
-        return "thanks"
-    else:
-        return req.text, req.status_code
+    try:
+        req = requests.post(url, data,
+                            headers=headers)
+        if req.status_code is 200:
+            return "thanks"
+        else:
+            return req.text, req.status_code
+    except Exception as e:
+        return json.dumps({'error': str(e)}), 400
 
 
 if __name__ == "__main__":
