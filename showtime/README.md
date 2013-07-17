7da6aad7543353d5d0dfbaf4c0cf8e93522b277a
Oliver Spindler <os@oliverspindler.com>
2013-07-17 17:56:13 +0100

    refactor, add Ubuntu support

--- showtime/README.md
+++ showtime/README.md
@@ -1,9 +1,37 @@
 # Showtime
 
-Webdriver + osxautomation mashup
+Webdriver + osxautomation / xaut mashup
 
 ## Installation and usage
 
- - Install https://github.com/joseph/osxautomation
- - sudo gem install watir-webdriver
- - ruby showtime.rb
+### OS X
+
+ - Install osxautomation: ```bash
+git clone https://github.com/joseph/osxautomation
+cd osxautomation
+scripts/buildrun
+sudo cp build/Release/osxautomation /usr/local/bin
+```
+
+ - `sudo gem install watir-webdriver`
+ 
+### Ubuntu
+ - Install Chromium, easiest from the Ubuntu Software Center
+ - Install Chromedriver:
+   - Download from https://code.google.com/p/chromedriver/downloads/list
+   - Unzip and put chromedriver into PATH
+ - Ensure rubygems is up-to-date: `sudo apt-get install rubygems`
+ - `sudo gem install watir-webdriver`
+ - Install xaut:
+   - Download xaut from http://sourceforge.net/projects/xautomation/
+   - Extract it
+   - ```bash
+cd <xaut_directory>
+./configure
+make
+cd python
+sudo python setup.py install
+```
+
+### Running journeys
+- `ruby showtime.rb`
--- showtime/showtime.rb
+++ showtime/showtime.rb
@@ -1,94 +1,71 @@
 require 'rubygems'
 require 'watir-webdriver'
 
-@b = Watir::Browser.new :chrome
+# check if osxautomation or xaut is installed
+`osxautomation`
+osxautomation_installed = $?.success?
+`python check_xaut.py`
+xaut_installed = $?.success?
+
+Dir["automators/*.rb"].each {|file| require file }
+if osxautomation_installed
+  @automator = OsxautomationAutomator.new
+elsif xaut_installed
+  @automator = XautAutomator.new
+else
+  @automator = BaseAutomator.new
+end
+
+
+@browser = Watir::Browser.new :chrome
+
+
 
 
 # calibrate
 
-@b.execute_script <<-JS
+@browser.execute_script <<-JS
   document.write('Showtime! Click into window to begin.');
   document.onclick = function(e){
     window.activated = true;
     window.cursorX = e.pageX;
     window.cursorY = e.pageY;
+    document.write('<br>Go!');
   }
 JS
 
 activated = false
 while !activated
-  activated = @b.execute_script <<-JS
+  activated = @browser.execute_script <<-JS
     return window.activated === true;
   JS
   sleep 0.1
 end
 
-posBrowser = @b.execute_script <<-JS
+
+posBrowser = @browser.execute_script <<-JS
   return [window.cursorX, window.cursorY];
 JS
-posGlobal = `osxautomation mouselocation`.split
-
-@offsetX = posGlobal[0].to_i - posBrowser[0].to_i
-@offsetY = posGlobal[1].to_i - posBrowser[1].to_i
-
-def moveMouse (x, y)
-  x += @offsetX
-  y += @offsetY
-  system( "osxautomation \"mousemove #{x} #{y}\" > /dev/null" )
-end
-
-def moveToEl(el)
-  lefttop = el.wd.location
-  size = el.wd.size
-  centre = [lefttop[0] + size[0] / 2, lefttop[1] + size[1] / 2]
-  rightbottom = [lefttop[0] + size[0], lefttop[1] + size[1]]
-  res = @b.execute_script <<-JS
-    return {
-      windowWidth: $(window).width(),
-      windowHeight: $(window).height(),
-      scrollTop: $(window).scrollTop(),
-      scrollLeft: $(window).scrollLeft(),
-    }
-  JS
-
-  scrollLeft = res['scrollLeft'].to_i
-  scrollTop = res['scrollTop'].to_i
-
-  visible_lefttop = [scrollLeft, scrollTop]
-  visible_rightbottom = [scrollLeft + res['windowWidth'].to_i, scrollTop + res['windowHeight'].to_i]
-
-  # is element fully visible?
-  visible_horizontal = visible_lefttop[0] <= lefttop[0] && rightbottom[0] <= visible_rightbottom[0]
-  visible_vertical = visible_lefttop[1] <= lefttop[1] && rightbottom[1] <= visible_rightbottom[1]
+posGlobal = @automator.mouse_location
+@offsets = {
+  :x => posGlobal[0].to_i - posBrowser[0].to_i,
+  :y => posGlobal[1].to_i - posBrowser[1].to_i
+}
 
 
-  if !visible_vertical
-    scrollTop = centre[1] - res['windowHeight'].to_i / 2
-    @b.execute_script <<-JS
-      $('html, body').animate({
-          scrollTop: #{scrollTop}
-        },
-        {
-          duration: 400,
-          easing: 'swing'
-        });
-    JS
-    sleep 0.5
-  end
-
-  moveMouse(centre[0] - scrollLeft, centre[1] - scrollTop)
-
-end
-
+Dir["journeys/*.rb"].each {|file| require file }
 
 journeys = [
-  'licensing'
+  LicensingJourney.new(@browser, @automator, @offsets)
 ]
 
 
-journeys.each do |journey|
-  @b.goto 'gov.uk/performance'
-  require "journeys/#{journey}"
+
+
+while true
+  journeys.each do |journey|
+    journey.run
+  end
 end
 
-@b.quit
+@browser.quit
